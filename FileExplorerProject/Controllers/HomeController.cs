f46f58e6779f0d08491b014f0643cdb30923c8a8
amanTesfa <amantesfa1010@gmail.com>
2026-03-08 21:10:10 +0300

    Project related issue fixed

--- FileExplorerProject/Controllers/HomeController.cs
+++ FileExplorerProject/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using WebApplication1.Models;
+using FileExplorerProject.Models;
 
-namespace WebApplication1.Controllers
+namespace FileExplorerProject.Controllers
 {
     public class HomeController : Controller
     {
--- FileExplorerProject/Models/ErrorViewModel.cs
+++ FileExplorerProject/Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1.Models
+namespace FileExplorerProject.Models
 {
     public class ErrorViewModel
     {
